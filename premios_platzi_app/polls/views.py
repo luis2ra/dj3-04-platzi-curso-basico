e5d75e564572ae8ae3d36768b243fa5500a25618
Luis <luis@blooz.pe>
2022-10-11 12:59:49 -0500

    class 19: adding view functions in the project

--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -2,6 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello World of Django")
+    print("Las vistas siempre reciben un parametro tipo <request>", type(request))
+    print("El request tiene un parametro <headers>: ", request.headers)
+    # print("El request tiene un parametro <data>: ", request.data)
+    return HttpResponse("Estas en la pagina principal de Premios Platzi!")
+
+
+def detail(request, question_id):
+    # print("El request tiene un parametro <data>: ", request.data)
+    return HttpResponse(f"Estas viendo la pregunta nro.: {question_id}")
+
+
+def results(request, question_id):
+    return HttpResponse(f"Estas viendo los resultados de la pregunta nro.: {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estas votando a la pregunta nro.: {question_id}")
