ea59ca71a2e1a1a64961437979957ae4017238ff
Luis Altuve <luis2ra@gmail.com>
2022-10-12 23:06:27 -0500

    class26: modifying the results function

--- premios_platzi_app/polls/views.py
+++ premios_platzi_app/polls/views.py
@@ -32,7 +32,14 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    return HttpResponse(f"Estas viendo los resultados de la pregunta nro.: {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(
+        request,
+        "polls/results.html",
+        {
+            "question": question
+        } 
+    )    
 
 
 def vote(request, question_id):
